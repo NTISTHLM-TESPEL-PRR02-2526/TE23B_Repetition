6f4b0423275629bedfcbf0faea4543701966f716
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2025-08-28 12:29:38 +0200

    etod & custom namn

--- Repetition/Program.cs
+++ Repetition/Program.cs
@@ -1,29 +1,36 @@
-﻿int p1Hp = 100;
-int p2Hp = 100;
+﻿Fight();
 
-string p1Name = "Alex";
-string p2Name = "Calin";
-
-while (p1Hp > 0 && p2Hp > 0)
+static void Fight()
 {
-  Console.WriteLine($"{p1Name}: {p1Hp}");
-  Console.WriteLine($"{p2Name}: {p2Hp}");
-  p2Hp -= Random.Shared.Next(10, 25); // Alex slår Calin
-  p1Hp -= Random.Shared.Next(10, 25); // Calin slår Alex
+  int p1Hp = 100;
+  int p2Hp = 100;
+
+  Console.WriteLine("Vad heter du?");
+  string p1Name = Console.ReadLine();
+
+  string p2Name = "Martin";
+
+  while (p1Hp > 0 && p2Hp > 0)
+  {
+    Console.WriteLine($"{p1Name}: {p1Hp}");
+    Console.WriteLine($"{p2Name}: {p2Hp}");
+    p2Hp -= Random.Shared.Next(10, 25); // Alex slår Calin
+    p1Hp -= Random.Shared.Next(10, 25); // Calin slår Alex
+    Console.ReadLine();
+  }
+
+  if (p1Hp > 0)
+  {
+    Console.WriteLine($"{p1Name} vann!");
+  }
+  else if (p1Hp <= 0 && p2Hp <= 0)
+  {
+    Console.WriteLine("Oavgjort!");
+  }
+  else
+  {
+    Console.WriteLine($"{p2Name} vann!");
+  }
+
   Console.ReadLine();
 }
-
-if (p1Hp > 0)
-{
-  Console.WriteLine($"{p1Name} vann!");
-}
-else if (p1Hp <= 0 && p2Hp <= 0)
-{
-  Console.WriteLine("Oavgjort!");
-}
-else
-{
-  Console.WriteLine($"{p2Name} vann!");
-}
-
-Console.ReadLine();
